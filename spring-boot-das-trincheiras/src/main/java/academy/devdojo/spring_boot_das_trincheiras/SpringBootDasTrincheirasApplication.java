ce805f2cc1b63629e229b1191f0109b7521d7ab2
santosandressa <andressasantosp0@gmail.com>
2024-08-11 17:59:45 -0300

    initial commit

---
+++ spring-boot-das-trincheiras/src/main/java/academy/devdojo/spring_boot_das_trincheiras/SpringBootDasTrincheirasApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.spring_boot_das_trincheiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDasTrincheirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDasTrincheirasApplication.class, args);
+	}
+
+}

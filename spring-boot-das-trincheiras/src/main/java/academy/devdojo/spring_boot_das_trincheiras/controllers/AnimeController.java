ee0aed7cbc367dac10c43b119f9c85b30a9f901e
santosandressa <andressasantosp0@gmail.com>
2024-08-12 22:18:56 -0300

    threads virtuais, stacktrace

--- spring-boot-das-trincheiras/src/main/java/academy/devdojo/spring_boot_das_trincheiras/controllers/AnimeController.java
+++ spring-boot-das-trincheiras/src/main/java/academy/devdojo/spring_boot_das_trincheiras/controllers/AnimeController.java
@@ -1,5 +1,6 @@
 package academy.devdojo.spring_boot_das_trincheiras.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/animes")
+@Slf4j
 public class AnimeController {
 
     @GetMapping
     public List<String> listAll() {
+        log.info(Thread.currentThread().getName());
         return List.of("Naruto", "One Piece", "Attack on Titan", "Love Hina");
     }
 }
